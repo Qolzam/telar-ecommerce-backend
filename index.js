7e564d7620eaece6a7d03938dedd95c407d6731d
amir <amir@telar.dev>
2025-07-22 23:40:53 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world! 🌍');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
