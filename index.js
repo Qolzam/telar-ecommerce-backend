5b1002a6c4d339af735a6a278c9d1d0851bcce22
amir <amir@telar.dev>
2025-07-22 23:48:08 +0900

    chore: add api example and cors config

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// middleware to handle CORS and JSON requests
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Hello, world! 🌍');
+});
+
+// Example route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the API!' });
 });
 
 app.listen(PORT, () => {
