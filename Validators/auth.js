c0e625c11068574572ff2839addf8c6a0fa072e2
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-07-28 21:23:01 +0530

    fix(#1): improve validation messages and structure in registerValidator

--- Validators/auth.js
+++ Validators/auth.js
@@ -1,20 +1,29 @@
 import { check } from 'express-validator';
 
 export const registerValidator = [
-  check('fullName').isString().notEmpty().withMessage('Full name is required'),
+  check('fullName')
+    .notEmpty()
+    .withMessage('Full name is required')
+    .bail()
+    .isString()
+    .withMessage('Full name must be a string'),
 
   check('email')
     .notEmpty()
     .withMessage('Email is required')
     .bail()
     .isString()
+    .withMessage('Email must be a string')
+    .bail()
     .isEmail()
     .withMessage('Email must be valid'),
 
   check('password')
     .notEmpty()
-    .isString()
     .withMessage('Password is required')
+    .bail()
+    .isString()
+    .withMessage('Password must be a string')
     .bail()
     .isLength({ min: 8 })
     .withMessage('Password must be at least 8 characters long')
