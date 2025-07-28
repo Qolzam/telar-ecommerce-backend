3280624edb634533b825f342123ed3643745eaf4
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-07-28 20:54:22 +0530

    fix(#1): remove unused 'next' parameter from error handler middleware

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-export const errorHandler = (error, req, res, next) => {
+export const errorHandler = (error, req, res) => {
   const code = res.code ? res.code : 500;
   res.status(code).json({ code, status: false, message: error.message, stack: error.stack });
 };
