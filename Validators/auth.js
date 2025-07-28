c7cb4daf00d08a26e6b38103a1627dbf5ca5d819
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-07-28 21:18:01 +0530

    fix(#1): enhance registerValidator

--- Validators/auth.js
+++ Validators/auth.js
@@ -1,17 +1,19 @@
 import { check } from 'express-validator';
 
 export const registerValidator = [
-  check('fullName').notEmpty().withMessage('Full name is required'),
+  check('fullName').isString().notEmpty().withMessage('Full name is required'),
 
   check('email')
     .notEmpty()
     .withMessage('Email is required')
     .bail()
+    .isString()
     .isEmail()
     .withMessage('Email must be valid'),
 
   check('password')
     .notEmpty()
+    .isString()
     .withMessage('Password is required')
     .bail()
     .isLength({ min: 8 })
