72e84d438de67171618edb700d72a6547148e628
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-02 12:53:39 +0530

    fix(#7): use validation.js file validations to validate registration

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,10 @@ import { Router } from 'express';
 import { authController } from '../controllers/index.js';
 import { registerValidator } from '../Validators/auth.js';
 import { validate } from '../Validators/validate.js';
+import { validateUserRegistration, handleValidationErrors } from '../Validators/validation.js';
 
 const router = Router();
 
-router.post('/register', registerValidator, validate, authController.register);
+router.post('/register', validateUserRegistration, handleValidationErrors, authController.register);
 
 export default router;
