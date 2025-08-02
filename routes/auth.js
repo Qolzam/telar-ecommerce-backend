8bb8e08865a170927be96cb96e3c7651a147ab4b
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-02 12:54:29 +0530

    refactor(#7): remove unneccessary file imports

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 
 import { authController } from '../controllers/index.js';
-import { registerValidator } from '../Validators/auth.js';
-import { validate } from '../Validators/validate.js';
 import { validateUserRegistration, handleValidationErrors } from '../Validators/validation.js';
 
 const router = Router();
