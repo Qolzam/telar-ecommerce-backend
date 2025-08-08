7be7e5a9ba8cf8f0c0372ce32d8c39a3bbcd69d7
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-08 18:49:50 +0530

    docs(#9): add docs for routes

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,10 @@ import {
 } from '../validators/validation.js';
 
 const router = Router();
-
+// POST /api/register - Register a user
 router.post('/register', validateUserRegistration, handleValidationErrors, authController.register);
+
+// POST /api/login - login user
 router.post('/login', validateUserLogin, handleValidationErrors, authController.login);
 
 export default router;
