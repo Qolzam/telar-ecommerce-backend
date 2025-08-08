410c60ca160da6812223dda0919d67d094836862
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-08 15:38:48 +0530

    feat(#9): add jwt expire time

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT, JWT_SECRET, DATABASE_URL } = process.env;
+const { PORT, JWT_SECRET, DATABASE_URL, JWT_EXPIRES_IN } = process.env;
 
 export const port = PORT;
 export const databaseUrl = DATABASE_URL;
 export const jwtSecret = JWT_SECRET;
+export const jwtExpiresIn = JWT_EXPIRES_IN;
