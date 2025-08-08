2dd734d4238a8599f0ccf164e8d2dc03c42e0d72
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-08 15:36:21 +0530

    feat(#9): add authentication middleware

---
+++ middlewares/isAuth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+
+import { jwtSecret } from '../config/keys';
+
+export const isAuth = async (req, res, next) => {
+  try {
+    const authorization = req.headers.authorization ? req.headers.authorization.split(' ') : [];
+    const token = authorization.length > 1 ? authorization[1] : null;
+
+    if (token) {
+      const payload = jwt.verify(token, jwtSecret);
+
+      if (payload) {
+        req.user = {
+          _id: payload._id,
+          email: payload.email,
+          fullName: payload.fullName,
+          role: payload.role
+        };
+      } else {
+        res.code = 401;
+        throw new Error('Unauthorized');
+      }
+    } else {
+      res.code = 400;
+      throw new Error('Token is required');
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
