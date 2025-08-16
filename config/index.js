3a93c0845bdaa0fff1f3640823647edc3d1be134
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-16 19:30:53 +0530

    feat(#11): add BCRYPT_ROUNDS and LOG_LEVEL to environment configuration

--- config/index.js
+++ config/index.js
@@ -2,8 +2,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT, JWT_SECRET, DATABASE_URL, JWT_EXPIRES_IN, SENDER_EMAIL, EMAIL_PASSWORD } =
-  process.env;
+const {
+  PORT,
+  JWT_SECRET,
+  DATABASE_URL,
+  JWT_EXPIRES_IN,
+  BCRYPT_ROUNDS,
+  SENDER_EMAIL,
+  EMAIL_PASSWORD,
+  LOG_LEVEL
+} = process.env;
 
 export const port = PORT;
 export const databaseUrl = DATABASE_URL;
@@ -11,3 +19,5 @@ export const jwtSecret = JWT_SECRET;
 export const jwtExpiresIn = JWT_EXPIRES_IN;
 export const senderEmail = SENDER_EMAIL;
 export const emailPassword = EMAIL_PASSWORD;
+export const bcryptRounds = BCRYPT_ROUNDS;
+export const logLevel = LOG_LEVEL;
