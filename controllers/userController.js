2f992caa6ab709587ce9d0b5de9c596170f348e8
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-24 09:41:56 +0530

    feat(#10): implement updateUser method

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,20 @@ const userController = {
     } catch (error) {
       next(error);
     }
+  },
+
+  updateUser: async (req, res, next) => {
+    try {
+      const { fullName, email, password } = req.body;
+      const updatedUser = await userService.updateUser(req.user.id, { fullName, email, password });
+      res.json({
+        status: true,
+        message: 'Profile updated successfully',
+        data: { user: updatedUser }
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 };
 
