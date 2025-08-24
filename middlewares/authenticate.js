9994307add9bb6d154965b65ee858cc0763a4b95
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-08-24 10:14:30 +0530

    feat(#10): implement authenticate middlware to reuse

---
+++ middlewares/authenticate.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+import { jwtSecret } from '../config';
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const authorization = req.headers.authorization ? req.headers.authorization.split(' ') : [];
+    const token = authorization.length > 1 ? authorization[1] : null;
+
+    if (token) {
+      const payload = jwt.verify(token, jwtSecret);
+
+      if (payload) {
+        req.user = {
+          _id: payload._id,
+          fullName: payload.fullName,
+          email: payload.email,
+          role: payload.role
+        };
+      } else {
+        return res.status(401).json({
+          status: false,
+          message: 'Unauthorized',
+          code: 'UNAUTHORIZED'
+        });
+      }
+    } else {
+      return res.status(400).json({
+        status: false,
+        message: 'Token is required',
+        code: 'BAD_REQUEST'
+      });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
